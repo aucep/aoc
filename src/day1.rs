4a3cd21a8857d63a58bb905c0d0b3589c4102c21
auceps <henscripting@gmail.com>
2022-12-01 16:25:47 -0500

    day1

--- src/day1.rs
+++ src/day1.rs
@@ -1,29 +1,27 @@
-use anyhow::{bail, Context, Result};
+use anyhow::{Context, Result};
+use itertools::Itertools;
 
 pub fn solution(input: &str) -> Result<(u32, u32)> {
-    let mut calorie_counts: Vec<u32> = input
-        .split("\n\n")
-        .filter(|group| !group.is_empty())
-        .map(|group| {
-            group
-                .lines()
-                .map(|line| line.parse::<u32>().context("encountered invalid calorie count"))
-                .sum()
-        })
+    let depths: Vec<u32> = input
+        .lines()
+        .map(|depth| depth.parse::<u32>().context("invalid depth measurement"))
         .collect::<Result<_>>()?;
 
-    calorie_counts.sort();
+    let part1 = depths
+        .iter()
+        .tuple_windows()
+        .filter(|(a, b)| a < b)
+        .count()
+        .try_into()?;
 
-    let [.., part1] = calorie_counts[..] else {
-        bail!("There should be at least 1 elf");
-    };
-
-    let part2 = {
-        let [.., a, b, c] = calorie_counts[..] else {
-            bail!("There should be at least 3 elves");
-        };
-        a + b + c
-    };
+    let part2 = depths
+        .iter()
+        .tuple_windows()
+        .map(|(a, b, c)| a + b + c)
+        .tuple_windows()
+        .filter(|(a, b)| a < b)
+        .count()
+        .try_into()?;
 
     Ok((part1, part2))
 }
@@ -34,20 +32,16 @@ mod tests {
 
     #[test]
     fn trivial_example() {
-        let input = "1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000";
-        assert_eq!((24000, 45000), solution(input).unwrap());
+        let input = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+        assert_eq!((7, 5), solution(input).unwrap());
     }
 }
