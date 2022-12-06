1ab610c4ca0c7e843c0b0bf2ec60fa5372219e01
auceps <78270279+aucep@users.noreply.github.com>
2022-12-06 06:06:39 +0000

    day6

---
+++ day6.nix
@@ -0,0 +1,21 @@
+with (import <nixpkgs> {}).lib;
+let
+	input = fileContents ./test_inputs/day6;
+	
+	windows = n: list:
+			genList (i: sublist i n list) (length list - n);
+	
+	firstUniqueSpan = n:
+		flip pipe [
+			stringToCharacters
+			(windows n)
+			(imap0 (i: xs: {inherit i xs;}))
+			(findFirst
+			 	({i, xs}: length (unique xs) == n)
+			 	(abort "no unique"))
+			({i, xs}: i + n)
+		];
+in {
+	part1 = firstUniqueSpan 4 input;
+	part2 = firstUniqueSpan 14 input;
+}
